4f52df218c96194c40b14aec73925db4a6cc9995
kyby91 <julienmoullec@yahoo.fr>
2022-12-07 22:22:46 +0100

    page home + reactDropDown

--- wealth-health/src/App.js
+++ wealth-health/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import './style.scss';
 
 function App() {
+
+  const options = [
+    'one', 'two', 'three'
+  ];
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>HRNET</h1>
+        <div>View Current Employees</div>
       </header>
+      <main>
+        <h2>Create employee</h2>
+        <div>
+            <form action="#" id="create-employee">
+              <label for="first-name"></label>
+              <input type="text" id="first-name" placeholder='First Name'/>
+
+              <label for="last-name"></label>
+              <input type="text" id="last-name" placeholder='Last Name'/>
+
+              <label for="date-of-birth"></label>
+              <input id="date-of-birth" type="text" placeholder='Date of Birth'/>
+
+              <label for="start-date"></label>
+              <input id="start-date" type="text" placeholder='Start Date'/>
+
+              <fieldset class="address">
+                  <legend>Address</legend>
+
+                  <label for="street"></label>
+                  <input id="street" type="text" placeholder='Street'/>
+
+                  <label for="city"></label>
+                  <input id="city" type="text" placeholder='City'/>
+
+                  <label for="state">State</label>
+                  <select name="state" id="state"></select>
+
+                  <label for="zip-code">Zip Code</label>
+                  <input id="zip-code" type="number" />
+              </fieldset>
+
+              <label for="department">Department</label>
+              <select name="department" id="department">
+                  <option>Sales</option>
+                  <option>Marketing</option>
+                  <option>Engineering</option>
+                  <option>Human Resources</option>
+                  <option>Legal</option>
+              </select>
+            </form>
+
+            <button onclick="saveEmployee()">Save</button>
+            <Dropdown options={options}  value='State' placeholder="Select an option" />;
+        </div>
+      </main>
     </div>
   );
 }
