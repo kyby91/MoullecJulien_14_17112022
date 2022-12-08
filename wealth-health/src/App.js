b2fbf73f9b1250da227e96bd5d18f71868b13f84
kyby91 <julienmoullec@yahoo.fr>
2022-12-08 09:59:08 +0100

    ajout plugin

--- wealth-health/src/App.js
+++ wealth-health/src/App.js
@@ -1,65 +1,293 @@
 import Dropdown from 'react-dropdown';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import 'react-dropdown/style.css';
 import './style.scss';
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
 
 function App() {
 
-  const options = [
-    'one', 'two', 'three'
+  const states = [
+    {
+        "name": "Alabama",
+        "abbreviation": "AL"
+    },
+    {
+        "name": "Alaska",
+        "abbreviation": "AK"
+    },
+    {
+        "name": "American Samoa",
+        "abbreviation": "AS"
+    },
+    {
+        "name": "Arizona",
+        "abbreviation": "AZ"
+    },
+    {
+        "name": "Arkansas",
+        "abbreviation": "AR"
+    },
+    {
+        "name": "California",
+        "abbreviation": "CA"
+    },
+    {
+        "name": "Colorado",
+        "abbreviation": "CO"
+    },
+    {
+        "name": "Connecticut",
+        "abbreviation": "CT"
+    },
+    {
+        "name": "Delaware",
+        "abbreviation": "DE"
+    },
+    {
+        "name": "District Of Columbia",
+        "abbreviation": "DC"
+    },
+    {
+        "name": "Federated States Of Micronesia",
+        "abbreviation": "FM"
+    },
+    {
+        "name": "Florida",
+        "abbreviation": "FL"
+    },
+    {
+        "name": "Georgia",
+        "abbreviation": "GA"
+    },
+    {
+        "name": "Guam",
+        "abbreviation": "GU"
+    },
+    {
+        "name": "Hawaii",
+        "abbreviation": "HI"
+    },
+    {
+        "name": "Idaho",
+        "abbreviation": "ID"
+    },
+    {
+        "name": "Illinois",
+        "abbreviation": "IL"
+    },
+    {
+        "name": "Indiana",
+        "abbreviation": "IN"
+    },
+    {
+        "name": "Iowa",
+        "abbreviation": "IA"
+    },
+    {
+        "name": "Kansas",
+        "abbreviation": "KS"
+    },
+    {
+        "name": "Kentucky",
+        "abbreviation": "KY"
+    },
+    {
+        "name": "Louisiana",
+        "abbreviation": "LA"
+    },
+    {
+        "name": "Maine",
+        "abbreviation": "ME"
+    },
+    {
+        "name": "Marshall Islands",
+        "abbreviation": "MH"
+    },
+    {
+        "name": "Maryland",
+        "abbreviation": "MD"
+    },
+    {
+        "name": "Massachusetts",
+        "abbreviation": "MA"
+    },
+    {
+        "name": "Michigan",
+        "abbreviation": "MI"
+    },
+    {
+        "name": "Minnesota",
+        "abbreviation": "MN"
+    },
+    {
+        "name": "Mississippi",
+        "abbreviation": "MS"
+    },
+    {
+        "name": "Missouri",
+        "abbreviation": "MO"
+    },
+    {
+        "name": "Montana",
+        "abbreviation": "MT"
+    },
+    {
+        "name": "Nebraska",
+        "abbreviation": "NE"
+    },
+    {
+        "name": "Nevada",
+        "abbreviation": "NV"
+    },
+    {
+        "name": "New Hampshire",
+        "abbreviation": "NH"
+    },
+    {
+        "name": "New Jersey",
+        "abbreviation": "NJ"
+    },
+    {
+        "name": "New Mexico",
+        "abbreviation": "NM"
+    },
+    {
+        "name": "New York",
+        "abbreviation": "NY"
+    },
+    {
+        "name": "North Carolina",
+        "abbreviation": "NC"
+    },
+    {
+        "name": "North Dakota",
+        "abbreviation": "ND"
+    },
+    {
+        "name": "Northern Mariana Islands",
+        "abbreviation": "MP"
+    },
+    {
+        "name": "Ohio",
+        "abbreviation": "OH"
+    },
+    {
+        "name": "Oklahoma",
+        "abbreviation": "OK"
+    },
+    {
+        "name": "Oregon",
+        "abbreviation": "OR"
+    },
+    {
+        "name": "Palau",
+        "abbreviation": "PW"
+    },
+    {
+        "name": "Pennsylvania",
+        "abbreviation": "PA"
+    },
+    {
+        "name": "Puerto Rico",
+        "abbreviation": "PR"
+    },
+    {
+        "name": "Rhode Island",
+        "abbreviation": "RI"
+    },
+    {
+        "name": "South Carolina",
+        "abbreviation": "SC"
+    },
+    {
+        "name": "South Dakota",
+        "abbreviation": "SD"
+    },
+    {
+        "name": "Tennessee",
+        "abbreviation": "TN"
+    },
+    {
+        "name": "Texas",
+        "abbreviation": "TX"
+    },
+    {
+        "name": "Utah",
+        "abbreviation": "UT"
+    },
+    {
+        "name": "Vermont",
+        "abbreviation": "VT"
+    },
+    {
+        "name": "Virgin Islands",
+        "abbreviation": "VI"
+    },
+    {
+        "name": "Virginia",
+        "abbreviation": "VA"
+    },
+    {
+        "name": "Washington",
+        "abbreviation": "WA"
+    },
+    {
+        "name": "West Virginia",
+        "abbreviation": "WV"
+    },
+    {
+        "name": "Wisconsin",
+        "abbreviation": "WI"
+    },
+    {
+        "name": "Wyoming",
+        "abbreviation": "WY"
+    }
   ];
 
+  let options = []
+  states.forEach(elm =>{
+    options.push(elm.name)
+  })
 
+  const department = [
+    'Sales', 'Marketing', 'Engineering', 'Human Resources', 'Legal'
+  ]
+  const defaultDepartment = department[0]
+
+  const [birthDate, setBirthDate] = useState(null);
+  const [startDate, setStartDate] = useState(null);
 
   return (
     <div className="App">
       <header>
         <h1>HRNET</h1>
-        <div>View Current Employees</div>
+        <Link to='/employee'>View Current Employees</Link>
       </header>
       <main>
         <h2>Create employee</h2>
         <div>
             <form action="#" id="create-employee">
-              <label for="first-name"></label>
-              <input type="text" id="first-name" placeholder='First Name'/>
-
-              <label for="last-name"></label>
+              <input type="text" id="first-name" placeholder='First Name' className='first-margin'/>
               <input type="text" id="last-name" placeholder='Last Name'/>
-
-              <label for="date-of-birth"></label>
-              <input id="date-of-birth" type="text" placeholder='Date of Birth'/>
-
-              <label for="start-date"></label>
-              <input id="start-date" type="text" placeholder='Start Date'/>
+              {/* <input id="date-of-birth" type="text" placeholder='Date of Birth'/> */}
+              <DatePicker selected={birthDate} onChange={(date) => setBirthDate(date)} placeholderText='Date of Birth'/>
+              {/* <input id="start-date" type="text" placeholder='Start Date'/> */}
+              <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} placeholderText='Start Date'/>
 
               <fieldset class="address">
                   <legend>Address</legend>
-
-                  <label for="street"></label>
                   <input id="street" type="text" placeholder='Street'/>
-
-                  <label for="city"></label>
                   <input id="city" type="text" placeholder='City'/>
-
-                  <label for="state">State</label>
-                  <select name="state" id="state"></select>
-
-                  <label for="zip-code">Zip Code</label>
-                  <input id="zip-code" type="number" />
+                  <Dropdown options={options}  value='State'  className='dropdown'/>
+                  <input id="zip-code" type="number" placeholder='Zip Code'/>
               </fieldset>
-
-              <label for="department">Department</label>
-              <select name="department" id="department">
-                  <option>Sales</option>
-                  <option>Marketing</option>
-                  <option>Engineering</option>
-                  <option>Human Resources</option>
-                  <option>Legal</option>
-              </select>
+              <Dropdown options={department} value={defaultDepartment} className='dropdown'/>
             </form>
 
-            <button onclick="saveEmployee()">Save</button>
-            <Dropdown options={options}  value='State' placeholder="Select an option" />;
+            <button  className='save'>Save</button>
         </div>
       </main>
     </div>
--- wealth-health/src/index.js
+++ wealth-health/src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import Employee from './employee.js'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='/employee' element={<Employee/>}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
